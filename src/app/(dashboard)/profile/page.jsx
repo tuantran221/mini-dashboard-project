7727812259892a5a1a94fc5b0b2fb7f1662ab285
tuantran22012001 <tuan.tran@inbox.xyz>
2023-09-14 16:57:25 +0700

    refactor profile page

--- src/app/(dashboard)/profile/page.jsx
+++ src/app/(dashboard)/profile/page.jsx
@@ -1,9 +1,6 @@
 "use client";
-import { useAuthContext } from "@/app/context/auth/AuthProvider";
 
 export default function Profile() {
-  const { user } = useAuthContext();
-
   const showUserName = () => {
     const userInf = JSON.parse(localStorage.getItem("token"));
     return <h2>{userInf.userName}</h2>;
