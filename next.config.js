0203d00e4e416fda966ba5ca2304b9eaf7484369
tuantran221 <trieutuan22012001@gmail.com>
2023-11-08 19:52:48 +0700

    fix config to deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+  // Add this line to your configuration
+  target: "experimental-serverless-trace",
+  distDir: "out",
+  exportPathMap: async function () {
+    return {
+      "/": { page: "/" },
+      "/about": { page: "/about" },
+      // Add any other pages you want to export
+    };
+  },
+};
